f6454e423eb8e7ba034bf10a747904de6abf61e7
Jomary Coquilla <jomarycoquilla465@gmail.com>
2025-01-01 19:06:13 +0800

    .

--- Collections and Tuple.py
+++ Collections and Tuple.py
@@ -3,3 +3,10 @@ from collections import Counter
 data = ['apple', 'banana', 'apple', 'orange', 'banana', 'apple']
 count = Counter(data)
 print(count)
+
+# Using defaultdict for handling missing keys:
+from collections import defaultdict
+d = defaultdict(int)
+d['apple'] += 1
+d['banana'] += 2
+print(d)
